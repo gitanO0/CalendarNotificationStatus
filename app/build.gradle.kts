d7dc54d59a5a928c6fd8db7afe75c459d601e302
royce_s <roycenlisa@gmail.com>
2026-05-19 10:46:09 -0600

    Release: Version bump to v0.94

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.royce.calendarnotificationstatus"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.93"
+        versionCode = 5
+        versionName = "0.94"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
